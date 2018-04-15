2030d68f9f90af8465f3f6f2949ea6efc9bff11c
Peter Richards <prichardsf@gmail.com>
2018-04-15 00:03:40 -0400

    Talk a little more backend

---
+++ assets/js/autocomplete.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   $('input.autocomplete').autocomplete({
+     data: {
+       "Mary": null,
+       "Mary Smith": null,
+       "Margret": null,
+       "Marget Jones": null,
+       "Mery Smith": null,
+       "Mergret": null,
+       "Merget Jones": null,
+       "Jane": null,
+       "Jack": null,
+       "Barnard Hall": null,
+       "Barnard Village": null
+     },
+   });
+ });
