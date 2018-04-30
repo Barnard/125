0dc160a600dc175bd1c6f82aa8a8d53d6105ec26
Peter Richards <prichardsf@gmail.com>
2018-04-30 18:53:46 -0400

    Rough draft of book uploaded

---
+++ narrative/BookReaderJSSimple.js
@@ -0,0 +1,714 @@
+//
+// This file shows the minimum you need to provide to BookReader to display a book
+//
+// Copyright(c)2008-2009 Internet Archive. Software license AGPL version 3.
+
+// Create the BookReader object
+var options = {
+  data: [
+
+
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-000.jpg' },
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-001.jpg' },
+    ],
+
+
+
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-002.jpg' },
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-003.jpg' },
+    ],
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-004.jpg' },
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-005.jpg' },
+    ],
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-006.jpg' },
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-007.jpg' },
+    ],
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-008.jpg' },
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-009.jpg' },
+    ],
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-010.jpg' },
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-011.jpg' },
+    ],
+    [
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-012.jpg'},
+      { width: 900, height: 1200,
+        uri: './pagejpgs/RCC-draft-013.jpg'},
+    ],
+
+
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-014.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-015.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-016.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-017.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-018.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-019.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-020.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-021.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-022.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-023.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-024.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-025.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-026.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-027.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-028.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-029.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-030.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-031.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-032.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-033.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-034.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-035.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-036.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-037.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-038.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-039.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-040.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-041.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-042.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-043.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-044.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-045.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-046.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-047.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-048.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-049.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-050.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-051.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-052.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-053.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-054.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-055.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-056.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-057.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-058.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-059.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-060.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-061.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-062.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-063.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-064.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-065.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-066.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-067.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-068.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-069.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-070.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-071.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-072.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-073.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-074.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-075.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-076.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-077.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-078.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-079.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-080.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-081.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-082.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-083.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-084.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-085.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-086.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-087.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-088.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-089.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-090.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-091.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-092.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-093.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-094.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-095.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-096.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-097.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-098.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-099.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-100.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-101.jpg'},
+],
+
+
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-102.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-103.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-104.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-105.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-106.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-107.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-108.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-109.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-110.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-111.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-112.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-113.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-114.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-115.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-116.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-117.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-118.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-119.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-120.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-121.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-122.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-123.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-124.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-125.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-126.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-127.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-128.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-129.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-130.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-131.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-132.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-133.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-134.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-135.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-136.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-137.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-138.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-139.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-140.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-141.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-142.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-143.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-144.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-145.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-146.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-147.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-148.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-149.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-150.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-151.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-152.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-153.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-154.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-155.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-156.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-157.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-158.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-159.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-160.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-161.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-162.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-163.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-164.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-165.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-166.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-167.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-168.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-169.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-170.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-171.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-172.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-173.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-174.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-175.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-176.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-177.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-178.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-179.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-180.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-181.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-182.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-183.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-184.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-185.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-186.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-187.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-188.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-189.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-190.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-191.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-192.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-193.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-194.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-195.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-196.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-197.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-198.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-199.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-200.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-201.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-202.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-203.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-204.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-205.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-206.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-207.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-208.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-209.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-210.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-211.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-212.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-213.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-214.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-215.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-216.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-217.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-218.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-219.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-220.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-221.jpg'},
+],
+[
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-222.jpg'},
+{ width: 900, height: 1200,
+uri: './pagejpgs/RCC-draft-223.jpg'},
+]
+
+
+  ],
+
+  // Book title and the URL used for the book title link
+  bookTitle: 'Draft of Barnard History Book',
+  bookUrl: 'https://barnardhistory.org',
+  bookUrlText: 'Back to Barnard History',
+  bookUrlTitle: 'https://barnardhistory.org',
+
+  // thumbnail is optional, but it is used in the info dialog
+  //thumbnail: '//archive.org/download/BookReader/img/page014.jpg',
+  // Metadata is optional, but it is used in the info dialog
+  metadata: [
+    {label: 'Title', value: 'Draft of Barnard History Book'},
+    {label: 'Author', value: 'Robert McCaughey'},
+    {label: ' Info', value: 'A draft of a book about the history of Barnard.'},
+  ],
+
+  ui: 'full', // embed, full (responsive)
+
+  el: '#BookReader',
+};
+var br = new BookReader(options);
+br.init();
